2a05aa85a5640f4f2b63b1913173a4b42fc8ffc7
wx14 <wx14@software.nju.edu.cn>
2016-03-11 23:08:04 +0800

    测试

--- src/main/java/data/Test.java
+++ src/main/java/data/Test.java
@@ -11,25 +11,10 @@ import po.UserPO;
 
 public class Test {
 	public static void main(String[] args) throws IOException{
-//		ArrayList<RepositoryPO> list=new RepositoryData().sort(Repository_Sort.fork);
-//		
-//		for(int i=0;i<list.size();i++){
-//			System.out.println(list.get(i).getForks());
-//		}
-//		UserPO po=new UserData().CheckUser("macournoyer");
-//		System.out.println(po.getEmail());
-//		ArrayList<UserPO> list=new UserData().getUser(); 
-//		System.out.println(list.get(2).getLogin());
-//		String s1,s2,s3;
-//		s1=new UserData().UserInfo("mojombo", UserInfo.login);
-//		s2=new UserData().UserInfo("technoweenie", UserInfo.email);
-//		s3=new UserData().UserInfo("macournoyer", UserInfo.name);
-//		System.out.println(s1);
-//		System.out.println(s2);
-//		System.out.println(s3);
-		ArrayList<UserPO> list=new UserData().search("mbo");
-		for(int i=0;i<list.size();i++){
-			System.out.println(list.get(i).getEmail());
+		RepositoryData testRepository=new RepositoryData();
+		ArrayList<String> a=testRepository.getRepositoriesNames();
+		for(int i=0;i<a.size();i++){
+			System.out.println(a.get(i));
 		}
 		
 				}
