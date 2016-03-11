a0ce6251b7df17e3414968fa876bcdba0457933c
jxf14 <jxf14@software.nju.edu.cn>
2016-03-11 14:41:47 +0800

    Development:修改

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,5 +1,8 @@
 import com.sun.java.swing.plaf.windows.WindowsLookAndFeel;
+
+import presentation.homeui.HomeFrame;
 import presentation.repoCheckui.RepCheckFrame;
+import presentation.searchui.RepInfoSubPanel;
 import presentation.userCheckui.UserCheckFrame;
 
 import javax.swing.*;
@@ -7,15 +10,22 @@ import javax.swing.*;
 /**
  * Created by moeyui on 2016/3/1 0001.
  */
-public class Test {
+
+public class Test extends JFrame{
+//	public Test(){
+//		this.setSize(430, 100);
+//		this.add(new RepInfoSubPanel(null, 400, 60));
+//		this.setVisible(true);
+//	}
     public static void main(String[] args) {
 //    	new SearchFrame();
-//        new HomeFrame();
-        try {
-            UIManager.setLookAndFeel(new WindowsLookAndFeel());
-        } catch (UnsupportedLookAndFeelException e) {
-            e.printStackTrace();
-        }
-        new RepCheckFrame();
+        new HomeFrame();
+//        try {
+//            UIManager.setLookAndFeel(new WindowsLookAndFeel());
+//        } catch (UnsupportedLookAndFeelException e) {
+//            e.printStackTrace();
+//        }
+//        new RepCheckFrame();
+//        new Test();
     }
 }
