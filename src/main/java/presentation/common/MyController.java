e28ad27c1e3541dcf0d950b30b67ec5646988802
myk14 <myk14@software.nju.edu.cn>
2016-03-16 00:24:26 +0800

    Development:add CheckUserController

---
+++ src/main/java/presentation/common/MyController.java
@@ -0,0 +1,7 @@
+package presentation.common;
+
+/**
+ * Created by moeyui on 2016/3/15 0015.
+ */
+public interface MyController {
+}
---
+++ src/main/java/presentation/userCheckui/CheckUserController.java
@@ -0,0 +1,7 @@
+package presentation.userCheckui;
+
+/**
+ * Created by moeyui on 2016/3/15 0015.
+ */
+public class CheckUserController {
+}
