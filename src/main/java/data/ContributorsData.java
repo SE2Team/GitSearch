e690bd4dcf38cd78fad1b86e90522b9f62ccdd31
ch14 <ch14@software.nju.edu.cn>
2016-03-23 22:16:43 +0800

    Development；merge数据层代码

--- src/main/java/data/ContributorsData.java
+++ src/main/java/data/ContributorsData.java
@@ -23,17 +23,10 @@ public class ContributorsData implements ContributorsDataService {
 		if(url.equals("")){
 			return null;
 		}
-		String string=data.getString1(url);
-		JSONArray obj1 = new JSONArray(string);
-		JSONObject obj = new JSONObject();
 		ArrayList<String> list=new ArrayList<String>();
-		for(int i=0;i<obj1.length();i++){
-			obj=obj1.getJSONObject(i);
-			list.add(obj.getString("login"));
-		}
+		list=data.getString(url);
 		return list;
 	}
 
 	
-
 }
--- src/main/java/data/UserData.java
+++ src/main/java/data/UserData.java
@@ -10,9 +10,10 @@ import java.util.ArrayList;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-
+import Util.Repository_Sort;
+import Util.User_Sort;
 import dataService.UserDataService;
-
+import po.RepositoryPO;
 import po.UserPO;
 
 /**
@@ -23,44 +24,17 @@ public class UserData implements UserDataService {
 	static String string = "http://www.gitmining.net/api/user";
 
 	public UserPO CheckUser(String user) throws IOException {
-		JSONObject obj = new JSONObject();
-	//	FileReader fr = new FileReader(new File("src/main/java/txtData/users.json"));
-		BufferedReader br = new BufferedReader(new InputStreamReader(this.getClass().
-				getResourceAsStream("/txtData/users.json")));
-		String string = br.readLine();
-		JSONArray obj1 = new JSONArray(string);
-		
-		for (int j = 0; j < obj1.length(); j++) {
-			obj = obj1.getJSONObject(j);
-			if (user.equals(obj.getString("login"))) {
-				String s1, s2, s3,s4;
-
-				if (obj.has("company")) {
-					s1 = obj.getString("company");
-				} else {
-					s1 = " ";
-				}
-				if (obj.has("email")) {
-					s2 = obj.getString("email");
-				} else {
-					s2 = "";
-				}
-
-				if (obj.has("repos_url")) {
-					s3 = obj.getString("repos_url");
-				} else {
-					s3 = "";
-				}
-
-				if (obj.has("name")) {
-					s4 = obj.getString("name");
-				} else {
-					s4 = "";
-				}
-				
-				return new UserPO(obj.getInt("id"), obj.getString("login"), obj.getString("type"),
-						s4, s1, s2, s3, obj.getInt("public_gists"), obj.getInt("followers"),
-						obj.getInt("following"), obj.getString("created_at"), obj.getString("updated_at"));
+		ArrayList<UserPO> list=new UserData().getUser();
+		ArrayList<String> list3=new ArrayList<String>();
+		for(int i=0;i<list.size();i++){
+			ArrayList<RepositoryPO> list2=new RepositoryData().Search(user);
+			for(int j=0;j<list2.size();j++){
+				list3.add(list2.get(j).getName());
+			}
+			
+			if(list.get(i).getLogin().equals(user)){
+				list.get(i).setHas(list3);
+				return list.get(i);
 			}
 		}
 		return null;
@@ -141,12 +115,14 @@ public class UserData implements UserDataService {
 		
 		ArrayList<UserPO> list = new ArrayList<UserPO>();
 		JSONObject obj = new JSONObject();
+		
 //		FileReader fr = new FileReader(new File("src/main/java/txtData/users.json"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(this.getClass().
 				getResourceAsStream("/txtData/users.json")));
 		String string = br.readLine();
 		JSONArray obj1 = new JSONArray(string);
 		for (int j = 0; j < obj1.length(); j++) {
+			
 			obj = obj1.getJSONObject(j);
 			String s1, s2, s3,s4;
 
@@ -169,13 +145,14 @@ public class UserData implements UserDataService {
 
 				if (obj.has("name")) {
 					s4 = obj.getString("name");
+					
 				} else {
 					s4 = "";
 				}
 				
 				list.add(new UserPO(obj.getInt("id"), obj.getString("login"), obj.getString("type"),
 						s4, s1, s2, s3, obj.getInt("public_gists"), obj.getInt("followers"),
-						obj.getInt("following"), obj.getString("created_at"), obj.getString("updated_at")));
+						obj.getInt("following"), obj.getString("created_at"), obj.getString("updated_at"),null,null));
 			}
 		
 
@@ -185,5 +162,35 @@ public class UserData implements UserDataService {
 	public ArrayList<String> NamesOfSubscriber(String userName, String reponame) throws IOException {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	public ArrayList<UserPO> sortUser(User_Sort sort) throws IOException {
+		// TODO Auto-generated method stub
+		ArrayList<UserPO> list=new UserData().getUser();
+		if(sort==User_Sort.Followers){
+			for(int j=0;j<list.size()-1;j++){
+				for(int i=j;i<list.size()-1;i++){
+					if(list.get(j).getFollowers()<=list.get(i+1).getFollowers()){
+						UserPO temp=list.get(j);
+						list.set(j, list.get(i+1));
+						list.set(i+1, temp);
+						
+						}
+					}
+				}
+		}else if(sort==User_Sort.Following){
+			for(int j=0;j<list.size()-1;j++){
+				for(int i=j;i<list.size()-1;i++){
+					if(list.get(j).getFollowing()<=list.get(i+1).getFollowing()){
+						UserPO temp=list.get(j);
+						list.set(j, list.get(i+1));
+						list.set(i+1, temp);
+						
+						}
+					}
+				}
+		}
+		
+		return list;
 	}
 }
