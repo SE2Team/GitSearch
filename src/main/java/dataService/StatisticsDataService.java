6d5f1624b7f97c86e9b56ae2f3c890f0ca0660ab
ch14 <ch14@software.nju.edu.cn>
2016-03-27 17:53:13 +0800

    Development:完善统计工作

--- src/main/java/dataService/StatisticsDataService.java
+++ src/main/java/dataService/StatisticsDataService.java
@@ -2,6 +2,7 @@ package dataService;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
 
 import po.RepositoryPO;
@@ -18,7 +19,7 @@ public interface StatisticsDataService {
 	 * over2000
 	 * @throws IOException 
 	 */
-	public ArrayList<Integer> getStar() throws IOException;
+	public Map<String, Integer> getStar() throws IOException;
 	
 	public StatisticsPO getScores(RepositoryPO po) throws IOException;
 	
@@ -31,14 +32,14 @@ public interface StatisticsDataService {
 	 * over1000
 	 * @throws IOException 
 	 */
-	public ArrayList<Integer> getForks() throws IOException;
+	public Map<String, Integer> getForks() throws IOException;
 	
 	/*
 	 * list中包含4个值，分别是从07到10每年创建的项目数
 	 * maxYear:20100721
 	 * minYear:20071029
 	 */
-	public ArrayList<Integer> getCreated() throws IOException;
+	public Map<String, Integer> getRepoCreated() throws IOException;
 	
 	/**
 	 *
@@ -49,4 +50,33 @@ public interface StatisticsDataService {
 	 *
 	 */
 	public Map<String, Integer> getLanguage() throws IOException;
+	
+	/**
+	 * list中包含9个值，分别是从07到15每年创建的用户数
+	 * maxYear:20151108
+	 * minYear:20071020
+	 * @return
+	 * @throws IOException 
+	 */
+	public Map<String, Integer>  getUserCreated() throws IOException;
+	
+	/**
+	 * 
+	 * 
+	 * @return 用户类型    数量
+	 * @throws IOException
+	 */
+	public Map<String, Integer> getUserType() throws IOException; 
+	
+	/**
+	 * 
+	 * @return 用户拥有的项目数  用户数量
+	 */
+	public Map<Integer, Integer> getUserHas();
+	
+	/**
+	 * 
+	 * @return 用户相关的项目数  用户数量
+	 */
+	public Map<Integer, Integer> getUserRelated();
 }
