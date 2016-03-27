a721095bfdfda1ea2b05654e8e6fb32b16549af1
myk14 <myk14@software.nju.edu.cn>
2016-03-27 22:15:48 +0800

    Development:增加排序

---
+++ src/main/java/presentation/searchui/ttttt.java
@@ -0,0 +1,29 @@
+package presentation.searchui;
+
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+/**
+ * Created by moeyui on 2016/3/27 0027.
+ */
+public class ttttt extends Application{
+
+    private Stage stage=new Stage();
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader=new FXMLLoader();
+        loader.setLocation(this.getClass().getResource("./test.fxml"));
+        AnchorPane anchorPane=(AnchorPane) loader.load();
+        testController controller=loader.getController();
+        Scene scene=new Scene(anchorPane);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
