23faea38c1d4e1ffcc6a2f418ddc7d34757da789
ch14 <ch14@software.nju.edu.cn>
2016-03-29 23:00:52 +0800

    Fix-bug:修复数据层外卖

--- src/main/java/data/Test.java
+++ src/main/java/data/Test.java
@@ -20,35 +20,20 @@ import po.UserPO;
 
 public class Test {
 	public static void main(String[] args) throws IOException{
-		
-//	for(int i=0;i<list2.size();i++){
-//			for(int p=i;p<list2.size()-1;p++){
-//				if(list2.get(i)>list2.get(p)){
-//					temp=list2.get(p);
-//					list2.set(p, list2.get(i));
-//					list2.set(i, temp);
-//	
-//				}
-//			}
-//		}
-//	从小到大排序
-		
-		
-		
-//		ArrayList<UserPO> list=new UserData().getUser();
-//		ArrayList<String> list2=new ArrayList<String>();
-//		for(int i=0;i<list.size();i++){
-//			if(list2.contains(list.get(i).getCompany())==false){
-//				list2.add(list.get(i).getCompany());
-//				System.out.println(list.get(i).getCompany());
-//			}
-//		}
-//		System.out.println(list2.size());
-		
-		ArrayList<RepositoryPO> list=new RepositoryData().Search("ai");
+		ArrayList<UserPO> list=new UserData().getUser();
+		UserPO po=null;
+		GetData get=new GetData("has");
+		UserData data=new UserData();
 		for(int i=0;i<list.size();i++){
-			System.out.println(list.get(i).getName());
+			po=data.CheckUser(list.get(i).getLogin());
+			get.writeDataAdd(po.getLogin()+";"+po.getHas().size());
 		}
 		
+		GetData get2=new GetData("related");
+		for(int i=0;i<list.size();i++){
+			po=data.CheckUser(list.get(i).getLogin());
+			get2.writeDataAdd(po.getLogin()+";"+po.getRelated().size());
+		}
+		System.out.println("Success");
 	}
 }
--- src/main/java/data/UserData.java
+++ src/main/java/data/UserData.java
@@ -1,8 +1,6 @@
 package data;
 
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
@@ -10,7 +8,6 @@ import java.util.ArrayList;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-import Util.Repository_Sort;
 import Util.User_Sort;
 import dataService.UserDataService;
 import po.RepositoryPO;
@@ -24,33 +21,33 @@ public class UserData implements UserDataService {
 	static String string = "http://www.gitmining.net/api/user";
 
 	public UserPO CheckUser(String user) throws IOException {
-		
-		ArrayList<UserPO> list=new UserData().getUser();
-		ArrayList<String> list3=new ArrayList<String>();
-		ArrayList<String> relatedRepo=new ArrayList<String>();
-		for(int i=0;i<list.size();i++){
+
+		ArrayList<UserPO> list = new UserData().getUser();
+		ArrayList<String> list3 = new ArrayList<String>();
+		ArrayList<String> relatedRepo = new ArrayList<String>();
+		for (int i = 0; i < list.size(); i++) {
 			if (list.get(i).getLogin().equals(user)) {
-				ArrayList<RepositoryPO> list2 = new RepositoryData().Search(user);
+				ArrayList<RepositoryPO> list2 = this.searchRepoName(user);
 				for (int j = 0; j < list2.size(); j++) {
 					list3.add(list2.get(j).getName());
 				}
-				
-				ArrayList<String>	related=new GetData("contributor").readData();
-				for(int p=0;p<related.size();p++){
-					if(related.get(p).contains(user)){
-						String[] str=related.get(p).split(";");
-						relatedRepo.add(str[str.length-1]);
+
+				ArrayList<String> related = new GetData("contributor").readData();
+				for (int p = 0; p < related.size(); p++) {
+					if (this.isEqual(related.get(p), user)) {
+						String[] str = related.get(p).split(";");
+						relatedRepo.add(str[str.length - 1]);
 					}
 				}
-				
-				ArrayList<String> colla=new GetData("collaborator").readData();
-				for(int p=0;p<colla.size();p++){
-					if(colla.get(p).contains(user)){
-						String[] str=colla.get(p).split(";");
-						relatedRepo.add(str[str.length-1]);
+
+				ArrayList<String> colla = new GetData("collaborator").readData();
+				for (int p = 0; p < colla.size(); p++) {
+					if (this.isEqual(colla.get(p), user)) {
+						String[] str = colla.get(p).split(";");
+						relatedRepo.add(str[str.length - 1]);
 					}
 				}
-				
+
 				list.get(i).setRelated(relatedRepo);
 				list.get(i).setHas(list3);
 				return list.get(i);
@@ -63,9 +60,10 @@ public class UserData implements UserDataService {
 	public String UserInfo(String user, Util.UserInfo info) throws IOException {
 		// TODO Auto-generated method stub
 		JSONObject obj = new JSONObject();
-	//	FileReader fr = new FileReader(new File("src/main/java/txtData/users.json"));
-		BufferedReader br = new BufferedReader(new InputStreamReader(this.getClass().
-				getResourceAsStream("/txtData/users.json")));
+		// FileReader fr = new FileReader(new
+		// File("src/main/java/txtData/users.json"));
+		BufferedReader br = new BufferedReader(
+				new InputStreamReader(this.getClass().getResourceAsStream("/txtData/users.json")));
 		String string = br.readLine();
 		String s1 = "";
 		JSONArray obj1 = new JSONArray(string);
@@ -107,72 +105,70 @@ public class UserData implements UserDataService {
 		return null;
 	}
 
-	// TODO Auto-generated method stub
-
 	public ArrayList<UserPO> search(String name) throws IOException {
 		// TODO Auto-generated method stub
-	//	FileReader fr = new FileReader(new File("src/main/java/txtData/user_names.txt"));
-	//	BufferedReader br = new BufferedReader(new Input);
+		// FileReader fr = new FileReader(new
+		// File("src/main/java/txtData/user_names.txt"));
+		// BufferedReader br = new BufferedReader(new Input);
 		ArrayList<UserPO> list = new UserData().getUser();
-	
+
 		UserPO po;
-		ArrayList<UserPO> list1=new ArrayList<UserPO>();
-		for(int i=0;i<list.size();i++){
-			po=list.get(i);
-			if(po.getLogin().contains(name)){
+		ArrayList<UserPO> list1 = new ArrayList<UserPO>();
+		for (int i = 0; i < list.size(); i++) {
+			po = list.get(i);
+			if (po.getLogin().contains(name)) {
 				list1.add(po);
 			}
 		}
-		
-	
+
 		return list1;
 	}
 
 	public ArrayList<UserPO> getUser() throws IOException {
 		// TODO Auto-generated method stub
-		
+
 		ArrayList<UserPO> list = new ArrayList<UserPO>();
 		JSONObject obj = new JSONObject();
-		
-//		FileReader fr = new FileReader(new File("src/main/java/txtData/users.json"));
-		BufferedReader br = new BufferedReader(new InputStreamReader(this.getClass().
-				getResourceAsStream("/txtData/users.json")));
+
+		// FileReader fr = new FileReader(new
+		// File("src/main/java/txtData/users.json"));
+		BufferedReader br = new BufferedReader(
+				new InputStreamReader(this.getClass().getResourceAsStream("/txtData/users.json")));
 		String string = br.readLine();
 		JSONArray obj1 = new JSONArray(string);
 		for (int j = 0; j < obj1.length(); j++) {
-			
+
 			obj = obj1.getJSONObject(j);
-			String s1, s2, s3,s4;
+			String s1, s2, s3, s4;
 
-				if (obj.has("company")) {
-					s1 = obj.getString("company");
-				} else {
-					s1 = "";
-				}
-				if (obj.has("email")) {
-					s2 = obj.getString("email");
-				} else {
-					s2 = "";
-				}
-
-				if (obj.has("repos_url")) {
-					s3 = obj.getString("repos_url");
-				} else {
-					s3 = "";
-				}
-
-				if (obj.has("name")) {
-					s4 = obj.getString("name");
-					
-				} else {
-					s4 = "";
-				}
-				
-				list.add(new UserPO(obj.getInt("id"), obj.getString("login"), obj.getString("type"),
-						s4, s1, s2, s3, obj.getInt("public_gists"), obj.getInt("followers"),
-						obj.getInt("following"), obj.getString("created_at"), obj.getString("updated_at"),null,null));
+			if (obj.has("company")) {
+				s1 = obj.getString("company");
+			} else {
+				s1 = "";
 			}
-		
+			if (obj.has("email")) {
+				s2 = obj.getString("email");
+			} else {
+				s2 = "";
+			}
+
+			if (obj.has("repos_url")) {
+				s3 = obj.getString("repos_url");
+			} else {
+				s3 = "";
+			}
+
+			if (obj.has("name")) {
+				s4 = obj.getString("name");
+
+			} else {
+				s4 = "";
+			}
+
+			list.add(new UserPO(obj.getInt("id"), obj.getString("login"), obj.getString("type"), s4, s1, s2, s3,
+					obj.getInt("public_gists"), obj.getInt("followers"), obj.getInt("following"),
+					obj.getString("created_at"), obj.getString("updated_at"), null, null));
+		}
 
 		return list;
 	}
@@ -184,31 +180,63 @@ public class UserData implements UserDataService {
 
 	public ArrayList<UserPO> sortUser(User_Sort sort) throws IOException {
 		// TODO Auto-generated method stub
-		ArrayList<UserPO> list=new UserData().getUser();
-		if(sort==User_Sort.Followers){
-			for(int j=0;j<list.size()-1;j++){
-				for(int i=j;i<list.size()-1;i++){
-					if(list.get(j).getFollowers()<=list.get(i+1).getFollowers()){
-						UserPO temp=list.get(j);
-						list.set(j, list.get(i+1));
-						list.set(i+1, temp);
-						
-						}
+		ArrayList<UserPO> list = new UserData().getUser();
+		if (sort == User_Sort.Followers) {
+			for (int j = 0; j < list.size() - 1; j++) {
+				for (int i = j; i < list.size() - 1; i++) {
+					if (list.get(j).getFollowers() <= list.get(i + 1).getFollowers()) {
+						UserPO temp = list.get(j);
+						list.set(j, list.get(i + 1));
+						list.set(i + 1, temp);
+
 					}
 				}
-		}else if(sort==User_Sort.Following){
-			for(int j=0;j<list.size()-1;j++){
-				for(int i=j;i<list.size()-1;i++){
-					if(list.get(j).getFollowing()<=list.get(i+1).getFollowing()){
-						UserPO temp=list.get(j);
-						list.set(j, list.get(i+1));
-						list.set(i+1, temp);
-						
-						}
+			}
+		} else if (sort == User_Sort.Following) {
+			for (int j = 0; j < list.size() - 1; j++) {
+				for (int i = j; i < list.size() - 1; i++) {
+					if (list.get(j).getFollowing() <= list.get(i + 1).getFollowing()) {
+						UserPO temp = list.get(j);
+						list.set(j, list.get(i + 1));
+						list.set(i + 1, temp);
+
 					}
+				}
+			}
+		}
+
+		return list;
+	}
+
+	/**
+	 * 按照用户名搜索项目
+	 * 
+	 * @param name
+	 * @return
+	 * @throws IOException
+	 */
+	private ArrayList<RepositoryPO> searchRepoName(String name) throws IOException {
+		ArrayList<RepositoryPO> list = new RepositoryData().getRepositories();
+		ArrayList<RepositoryPO> list2 = new ArrayList<RepositoryPO>();
+		String[] str = new String[2];
+		for (int i = 0; i < list.size(); i++) {
+			str = list.get(i).getName().split("/");
+			if (str[0].equals(name)) {
+				list2.add(list.get(i));			
 				}
 		}
-		
-		return list;
+		return list2;
+	}
+
+	private boolean isEqual(String str, String user) {
+		String[] strs = str.split(";");
+		for (int i = 0; i < strs.length - 1; i++) {
+			if (strs[i].length() > 2) {
+				if (strs[i].substring(1, strs[i].length() - 1).equals(user)) {
+					return true;
+				}
+			}
+		}
+		return false;
 	}
 }
