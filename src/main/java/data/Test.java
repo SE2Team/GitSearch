7984304e9579cc389a61d2f6ae2e21ccbda8fb69
ch14 <ch14@software.nju.edu.cn>
2016-04-05 19:58:57 +0800

    Development:增加数据层条件筛选接口和实现

--- src/main/java/data/Test.java
+++ src/main/java/data/Test.java
@@ -6,6 +6,7 @@ import java.util.Iterator;
 import java.util.Map;
 import java.util.Map.Entry;
 
+import Util.Repository_Sort;
 import Util.User_Sort;
 import businesslogic.userBL.User;
 
@@ -21,15 +22,13 @@ import po.UserPO;
 
 public class Test {
 	public static void main(String[] args) throws IOException{
-		UserData userData=new UserData();
-		UserPO po=null;
-		ArrayList<UserPO> list=new UserData().getUser();
-		System.out.println("Success");
+		ArrayList<RepositoryPO> list=new RepositoryData().getRepositories();
+		list=new RepositoryData().screenTime("200900");
+		list=new RepositoryData().screenLanguage("Ruby");
+	
 		for(int i=0;i<list.size();i++){
-			po=userData.CheckUser(list.get(i).getLogin());
-			new GetData("related").writeDataAdd(list.get(i).getLogin()+";"+po.getRelated().size());
+			System.out.println(list.get(i).getLanguage()+":"+list.get(i).getStargazers());
 			
 		}
-		System.out.println("Success");
 	}
 }
