f6e224f2eec49a6b15238952906daea4a9c0c2f5
myk14 <myk14@software.nju.edu.cn>
2016-04-15 23:52:30 +0800

    Fix bug: 迭代二文件路径修复

--- src/main/java/data/GetData.java
+++ src/main/java/data/GetData.java
@@ -1,14 +1,6 @@
 package data;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
@@ -116,7 +108,7 @@ public class GetData {
 		public ArrayList<String> readData() {
 			ArrayList<String> lists = new ArrayList<String>();
 			try {
-				BufferedReader br = new BufferedReader(new InputStreamReader(this.getClass().getResourceAsStream(path+".txt")));
+				BufferedReader br = new BufferedReader(new InputStreamReader(this.getClass().getResourceAsStream("/txtData/"+path+".txt")));
 				String temp = null;
 				while((temp = br.readLine()) != null) {
 					lists.add(temp);
--- src/main/java/data/Test.java
+++ src/main/java/data/Test.java
@@ -1,27 +1,26 @@
 package data;
 
 
+import po.RepositoryPO;
+
 import java.io.IOException;
 import java.util.ArrayList;
-
-import po.RepositoryPO;
-import po.StaStrPO;
 
 
 public class Test {
 	public static void main(String[] args) throws IOException{
 
 		
-		Refresh refresh=new Refresh();
+//		Refresh refresh=new Refresh();
 //		refresh.updatedLanguageSituation();
 //		refresh.updateCollaCount();
 //		refresh.updateContrCount();
 //		refresh.updatedC();
 //		refresh.updateHas();
 //		refresh.updateRelated();
-//		ArrayList<RepositoryPO> list=new RepositoryData().getRepositories();
+		ArrayList<RepositoryPO> list=new RepositoryData().getRepositories();
 //		StaStrPO po=new StatisticsData().getScores(list.get(0));
 //		System.out.println(po.getInt().get(2));
-		System.out.println("Success");
+		System.out.println(list.get(0).getCollaborators_count());
 	}
 }
